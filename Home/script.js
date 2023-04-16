93910e0584356bec2bdd284eec364a5e701741d3
tippytoe97 <tippileung1121@gmail.com>
2023-04-15 20:02:04 -0700

    completed building ver1 homepage

--- Home/script.js
+++ Home/script.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
  
     $("#owl-demo").owlCarousel({
    
-        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        autoPlay: 4000, //Set AutoPlay to 4 seconds
    
-        items : 4,
+        items : 3,
         itemsDesktop : [1199,3],
         itemsDesktopSmall : [979,3]
    
